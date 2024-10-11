5c7f4050a4769286fe67ca34e28236979989c45a
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-10-11 09:58:47 +0530

    Validate edit book form

--- routes/books.router.js
+++ routes/books.router.js
@@ -1,10 +1,11 @@
 const Router = require("express");
 const booksController = require("../controllers/books.controller");
+const { validateAddBook } = require("../middlewares/expressValidation");
 
 const booksRouter = Router()
 
 booksRouter.get("/:bookid", booksController.showBookDetail)
 booksRouter.get("/editBook/:bookid", booksController.showEditBook)
-booksRouter.post("/editBook/:bookid", booksController.editBook)
+booksRouter.post("/editBook/:bookid", validateAddBook, booksController.editBook)
 
 module.exports = booksRouter
